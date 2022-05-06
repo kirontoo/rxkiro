86ced64e7f88a5e10215c241ea7d71396832c4f9
Amy Dang <kirontoo@gmail.com>
2022-05-06 16:24:21 -0700

    Extract bot into its own class
    Creates a new bot using environment variabls, connects streamer chat,
    and a postgres database
    
    If a command is not found local, it will query a db for the command

---
+++ bot/bot.go
@@ -0,0 +1,82 @@
+package bot
+
+import (
+	"encoding/json"
+
+	"github.com/gempir/go-twitch-irc/v3"
+	"github.com/kirontoo/rxkiro/config"
+	"github.com/kirontoo/rxkiro/db"
+	"github.com/rs/zerolog"
+	"github.com/supabase/postgrest-go"
+)
+
+type Command struct {
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	Counter   int64  `json:"counter"`
+	Value     string `json:"value"`
+	IsCounter bool   `json:"isCounter"`
+	CreatedAt string `json:"created_at"`
+}
+
+type RxKiro struct {
+	Client   *twitch.Client
+	Config   config.Config
+	Commands map[string]interface{}
+	Log      zerolog.Logger
+	db       postgrest.Client
+}
+
+func NewBot(envPath string, logger zerolog.Logger) *RxKiro {
+	botConfig, err := config.LoadConfig(envPath, "bot")
+	if err != nil {
+		logger.Error().Msgf("%s", err)
+		logger.Fatal().Msg("Could not load env variables")
+	}
+
+	return &RxKiro{
+		Commands: commands,
+		Log:      logger,
+		Config:   botConfig,
+		Client:   twitch.NewClient(botConfig.BotName, botConfig.AuthToken),
+		db:       db.Connect(botConfig.DbUrl, botConfig.DbToken),
+	}
+}
+
+var commands = map[string]interface{}{
+	"ping": func(b *RxKiro) {
+		b.Send("pong")
+		b.Send("where is this going?")
+	},
+}
+
+func (b *RxKiro) Send(msg string) {
+	b.Client.Say(b.Config.Streamer, msg)
+}
+
+func (b *RxKiro) RunCmd(cmdName string) {
+	cmd, ok := b.Commands[cmdName]
+	if ok {
+		cmd.(func(*RxKiro))(b)
+	} else {
+		res, _, err := b.db.From("Commands").Select("*", "exact", false).Eq("name", cmdName).Execute()
+		if err != nil {
+			b.Log.Error().Msg(err.Error())
+			return
+		}
+
+		var dbCmd []Command
+		json.Unmarshal(res, &dbCmd)
+		b.Log.Debug().Interface("commands", dbCmd).Send()
+		if len(dbCmd) > 0 {
+			data := dbCmd[0]
+			if data.IsCounter {
+				// TODO
+			}
+
+			b.Send(data.Value)
+			// TODO cache db queries?
+		}
+	}
+
+}
