ecaf86f932026efcf49d434ca50da11663861167
Amy Dang <kirontoo@gmail.com>
2022-06-22 20:53:24 -0700

    Add more environment vars

--- config/config.go
+++ config/config.go
@@ -3,11 +3,16 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	BotName   string `mapstructure:"BOT_NAME"`
-	Streamer  string `mapstructure:"STREAMER"`
-	AuthToken string `mapstructure:"AUTH_TOKEN"`
-	DbUrl     string `mapstructure:"DB_API_URL"`
-	DbToken   string `mapstructure:"DB_TOKEN"`
+	BotName    string `mapstructure:"BOT_NAME"`
+	Streamer   string `mapstructure:"STREAMER"`
+	AuthToken  string `mapstructure:"AUTH_TOKEN"`
+	DbUrl      string `mapstructure:"DB_API_URL"`
+	DbToken    string `mapstructure:"DB_TOKEN"`
+	DbHost     string `mapstructure:"DB_HOST"`
+	DbUser     string `mapstructure:"DB_USER"`
+	DbPassword string `mapstructure:"DB_PASSWORD"`
+	DbPort     string `mapstructure:"DB_PORT"`
+	DbName     string `mapstructure:"DB_NAME"`
 }
 
 func LoadConfig(path string, configName string) (config Config, err error) {
